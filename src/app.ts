8263f0485ab2c2863ec120f1cfe266d375da8522
Forest Carlisle <forestcarlisle@gmail.com>
2016-12-08 13:40:53 -0800

    Added basic UI

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,20 @@
+import { autoinject } from 'aurelia-framework';
+import { Router, RouterConfiguration } from 'aurelia-router';
+
+@autoinject
 export class App {
-  message = 'Hello World!';
+
+  constructor() { }
+
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.title = 'Swollen';
+    config.map([
+      { route: ['','dashboard'], name: 'dashboard', moduleId: 'dashboard/dashboard', nav: true, title: 'Leader Board' },
+      { route: 'track', name: 'track', moduleId: 'track/track', nav: true, title: 'Track' }
+    ]);
+
+    this.router = router;
+  }
 }
